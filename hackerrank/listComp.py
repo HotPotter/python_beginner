f52b062b4ab35734285ba8ec81eeefe0a9e6c11a
Yan Dou <HotPotter@users.noreply.github.com>
2017-09-05 22:32:14 +0200

    list comprehesion

---
+++ hackerrank/listComp.py
@@ -0,0 +1,19 @@
+matrix = [
+[1, 2, 3, 4],
+[5, 6, 7, 8],
+[9, 10, 11, 12],
+]
+
+matrix_a = [row for row in matrix]
+
+print(matrix_a)
+
+
+
+# newMatrix = []
+# for col_index in range(0,len(matrix[0])):
+#     newRow = []
+#     for row in matrix:
+#         newRow.append(row[col_index])
+#     newMatrix.append(newRow)
+# print(newMatrix)

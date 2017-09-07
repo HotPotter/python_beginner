64df3a4419206fe61ee7acced10e75025d5f7666
Yan Dou <HotPotter@users.noreply.github.com>
2017-09-08 01:09:08 +0200

    whatever#

---
+++ hackerrank/recursive2.py
@@ -0,0 +1,10 @@
+import os
+def gci(filepath):
+#遍历filepath下所有文件，包括子目录
+  files = os.listdir(filepath)
+  for fi in files:
+    fi_d = os.path.join(filepath,fi)
+    if os.path.isdir(fi_d):
+      gci(fi_d)
+    else:
+      print os.path.join(filepath,fi_d)

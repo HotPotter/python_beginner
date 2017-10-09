bb7a80039baa32dcbf4763bee0210297e84cf8d8
yan <yan.dou@king.com>
2017-10-09 10:30:40 +0200

    add 1

---
+++ Hackerrank/collections_ordereddict.py
@@ -0,0 +1,7 @@
+from collections import OrderedDict
+d = OrderedDict()
+for _ in range(int(input())):
+    item, space, quantity = input().rpartition(' ') # rpartition(sep) returns a 3-tuple that contains the part before the separator, the separator and the part after the separator
+    d[item] = d.get(item, 0) + int(quantity) #get() returns a value of the given key
+for item, quantity in d.items():
+    print(item, quantity)

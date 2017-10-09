e861078cac615b0ba1bd3c6f04a1400c3062139b
yan <yan.dou@king.com>
2017-10-09 18:12:13 +0200

    deque

---
+++ Hackerrank/collections_pilingup.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+#num_case = input()
+#for _ in range(num_case)
+def inp():
+    d_len = input()
+    d_inp = input()
+    return d_len, d_inp
+
+
+def d_create(d_len, d_inp):
+    d_deque = deque(map(int,d_inp.split()))
+    return d_deque
+
+def pile(d_deque):
+    while max(d_deque) in (d_deque[0] or d_deque[-1]):
+        d_deque.remove(max(d_deque))
+    if len(d_deque)==0:
+        print("Yes")
+    else:
+        print("No")
+
+
+def go():
+    d_len,d_inp = inp()
+    d_deque = d_create()
+    pile()
+
+go()
+
